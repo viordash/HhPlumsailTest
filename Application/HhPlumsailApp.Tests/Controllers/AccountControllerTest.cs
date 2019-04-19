aed3c14b7e784e6aed1fba35be1a4fe8eb8d6bb4
Vitaliy Orazov <vitaly.orazov@rubetek.com>
2019-04-19 20:35:21 +0300

    WIP. add server-side tests

---
+++ Application/HhPlumsailApp.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using HhPlumsailApp.Controllers;
+using HhPlumsailApp.Models;
+using HhPlumsailApp.Services;
+using Microsoft.AspNet.Identity;
+using Moq;
+using NUnit.Framework;
+
+namespace HhPlumsailApp.Tests.Controllers {
+	[TestFixture]
+	public class AccountControllerTest {
+		AccountController testable;
+		Mock<IUserManagerService> userManagerServiceMock;
+
+		[SetUp]
+		public virtual void SetUp() {
+			userManagerServiceMock = new Mock<IUserManagerService>();
+			testable = new AccountController(userManagerServiceMock.Object);
+		}
+
+		[TearDown]
+		public virtual void TearDown() {
+			testable.Dispose();
+		}
+
+		#region Login
+		[Test]
+		public void Register_ReturnsResultOk() {
+			userManagerServiceMock
+				.Setup(x => x.RegisterUser(It.IsAny<UserModel>()))
+				.Returns(() => {
+					return Task.FromResult(IdentityResult.Success);
+				});
+
+			testable.ModelState.Clear();
+			var result = testable.Register(new UserModel() {
+				UserName = "test",
+				Password = "123456",
+				ConfirmPassword = "123456"
+			});
+			Assert.That(result, Is.TypeOf<Task<IHttpActionResult>>());
+			Assert.That(result.Result, Is.TypeOf<System.Web.Http.Results.OkResult>());
+		}
+		#endregion
+	}
+}
