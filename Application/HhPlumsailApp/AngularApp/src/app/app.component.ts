7375f101d125aa985495b1b14d99ce360344e0fb
Vitaliy Orazov <vitaly.orazov@rubetek.com>
2019-04-20 23:05:20 +0300

    add Angular project

---
+++ Application/HhPlumsailApp/AngularApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'Plumsail. A programming task for candidates for developer JavaScript, C#';
+}
