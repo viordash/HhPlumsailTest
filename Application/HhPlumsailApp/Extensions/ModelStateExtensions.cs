1336cb1d8abf6534ae0410ac03b1523c924e9b8d
Vitaliy Orazov <vitaly.orazov@rubetek.com>
2019-04-20 23:03:30 +0300

    tail of [c8fe875]. change ModelState validation to throws validation exception. Remove method GetErrorResult from controller(s). Use the exceptions throws and handle it's in ExceptionHandler

---
+++ Application/HhPlumsailApp/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Http.ModelBinding;
+
+namespace HhPlumsailApp.Extensions {
+	public static class ModelStateExtensions {
+		public static void Validate(this ModelStateDictionary modelState) {
+			if(modelState.IsValid) {
+				return;
+			}
+			var exception = new ValidationException();
+			exception.Data.Add(string.Empty, modelState.ToDictionary(k => k.Key, v => v.Value.Errors.Select(x => x.ErrorMessage)));
+			throw exception;
+		}
+	}
+}
