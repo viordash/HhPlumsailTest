2f26059638c3d3e858dd1bf3a285c38c6ba75bb4
Vitaliy Orazov <vitaly.orazov@rubetek.com>
2019-04-22 17:42:08 +0300

    ng. edit order as modal form

--- Application/HhPlumsailApp/AngularApp/src/app/orders/edit-order/edit-order.component.ts
+++ Application/HhPlumsailApp/AngularApp/src/app/orders/edit-order/edit-order.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { HttpClientService } from 'src/app/http-client.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs/internal/Subscription';
+import { Router } from '@angular/router';
 import { OrderModel } from '../orderModel';
 import { CustomerModel } from 'src/app/customers/CustomerModel';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	selector: 'app-edit-order',
@@ -12,31 +12,29 @@ import { CustomerModel } from 'src/app/customers/CustomerModel';
 	styleUrls: ['./edit-order.component.scss']
 })
 export class EditOrderComponent implements OnInit {
-	private id?: number;
-	private subscription: Subscription;
-	private order: OrderModel;
-	private customers: CustomerModel[];
+	@Input() orderId: number;
+	order: OrderModel;
+	customers: CustomerModel[];
 
 	private get isNew(): boolean {
-		return !!!this.id;
+		return !!!this.orderId;
+	}
+	get title(): string {
+		return this.isNew ? "Create new order" : "Edit order"
 	}
 
-	constructor(private activateRoute: ActivatedRoute, private httpClientService: HttpClientService, private router: Router) {
-		this.subscription = activateRoute.params.subscribe(params => {
-			this.id = params['id'];
-			if (this.id) {
-				this.httpClientService.getOrder(this.id)
-					.subscribe(result => {
-						this.order = result;
-					});
-			} else {
-				this.order = {} as OrderModel;
-			}
-		});
-
-	}
+	constructor(private httpClientService: HttpClientService, public activeModal: NgbActiveModal, private router: Router) { }
 
 	ngOnInit() {
+		if (this.orderId) {
+			this.httpClientService.getOrder(this.orderId)
+				.subscribe(result => {
+					this.order = result;
+				});
+		} else {
+			this.order = {} as OrderModel;
+		}
+
 		this.httpClientService.getCustomers()
 			.subscribe(result => {
 				this.customers = result;
@@ -44,17 +42,16 @@ export class EditOrderComponent implements OnInit {
 	}
 
 	saveOrder() {
-		if (!confirm(this.isNew ? "Create order?" : "Save order?")) {
-			return;
-		}
 		if (this.isNew) {
 			this.httpClientService.createOrder(this.order)
 				.subscribe(result => {
+					this.activeModal.close();
 					this.router.navigate(['/orders'])
 				});
 		} else {
-			this.httpClientService.saveOrder(this.id, this.order)
+			this.httpClientService.saveOrder(this.orderId, this.order)
 				.subscribe(result => {
+					this.activeModal.close();
 					this.router.navigate(['/orders'])
 				});
 		}
--- Application/HhPlumsailApp/AngularApp/src/app/orders/list-orders/list-orders.component.ts
+++ Application/HhPlumsailApp/AngularApp/src/app/orders/list-orders/list-orders.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClientService } from 'src/app/http-client.service';
 import { OrderModel } from '../orderModel';
+import { NgbModal, NgbModalConfig, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { EditOrderComponent } from '../edit-order/edit-order.component';
 
 @Component({
-  selector: 'app-list-orders',
-  templateUrl: './list-orders.component.html',
-  styleUrls: ['./list-orders.component.scss']
+	selector: 'app-list-orders',
+	templateUrl: './list-orders.component.html',
+	styleUrls: ['./list-orders.component.scss']
 })
 export class ListOrdersComponent implements OnInit {
-  private orders: OrderModel[];
+	private orders: OrderModel[];
 
-  constructor(private httpClientService: HttpClientService) { }
+	constructor(private httpClientService: HttpClientService, private modalService: NgbModal) { }
 
-  ngOnInit() {
-    this.httpClientService.getOrders()
-      .subscribe(result => {
-        this.orders = result;
-      });
-  }
+	ngOnInit() {
+		this.httpClientService.getOrders()
+			.subscribe(result => {
+				this.orders = result;
+			});
+	}
 
-  editItem(orderId: number) {
-  }
+	editItem(orderId: number) {
+		const config: NgbModalOptions = {
+			backdrop: 'static',
+			keyboard: false,
+			size: 'lg'
+		}
+		const modalRef = this.modalService.open(EditOrderComponent, config);
+		modalRef.componentInstance.orderId = orderId;
+	}
+
 }
