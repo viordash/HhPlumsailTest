3f6be6b8041ecf14d90321553376b498ee8263c2
Vitaliy Orazov <vitaly.orazov@rubetek.com>
2019-04-23 23:35:00 +0300

    remove unnecessary code

--- Application/HhPlumsailApp/Services/IAuthorizationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading.Tasks;
-
-namespace HhPlumsailApp.Services {
-	public interface IAuthorizationService {
-		Task<IEnumerable<Claim>> AuthorizeUserAsync(string userName, string password);
-	}
-}
